80b9a059f28f949e507bf3d59a3395738702112a
thotaveeru <tvrn.83@gmail.com>
2026-09-23 21:35:24 -0400

    Create HelloWorld.py with introductory content
    Add introductory Python program to print a message.

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+# This program is for intro of Python for the participants
+#print("HI Everyone, Welcome to Python learning Journey")
+
+person = "Generate content for python"
+
+for i in range(10):
+    print(person)
+    print("A")
